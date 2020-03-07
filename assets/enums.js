5e710ba836bf88d92b00831296a3043fb7cff01b
Greg <gkatechis@zendesk.com>
2020-03-06 21:13:02 -0600

    Adding enums.js

---
+++ assets/enums.js
@@ -0,0 +1,43 @@
+const dev_platform_feature = {
+    API: {
+        SUPPORT: 100,
+        CHAT: 101,
+        TALK: 102,
+        CHAT_CONVERSATIONS: 103,
+        GUIDE: 104,
+        SELL: 105
+    },
+    APPS_FRAMEWORK_DEVELOPMENT: {
+        SUPPORT: 120,
+        CHAT: 121,
+        SELL: 122
+    },
+    MOBILE_SDK: {
+        CHAT: {
+            IOS: 140,
+            ANDROID: 141
+        },
+        SUPPORT: {
+            IOS: 142,
+            ANDROID: 143
+        }
+    },
+    SUNSHINE: {
+        CUSTOM_OBJECTS: 160,
+        EVENTS_AND_PROFILES: 161,
+        CONVERSATIONS: 162,
+        AWS_EVENTS_BRIDGECHANNELS_FRAMEWORK: 163
+    },
+    WIDGET: {
+        API: 180,
+        WEB_SDK: 181
+    },
+    OTHER: {
+        SPONSORED_DEV_ACCOUNT: 200,
+        GETTING_APP_PUBLISHED: 201,
+        GLOBAL_OAUTH_CLIENT: 202,
+        API_CLIENTS: 203,
+        GUIDE_GATHER_CUSTOMIZATION: 204,
+        OTHER: 205
+    }
+}
